14e3202a5d79a21b7cf667ce93a4d3cd90769224
AdrianBottini <adrianboh@gmail.com>
2023-02-08 11:49:12 +0100

    mod

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 class LeadController extends Controller
 {
     /**
